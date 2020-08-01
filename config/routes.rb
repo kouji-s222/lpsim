050078485b02e0deef61e7c3af0726d534de7c9e
kouji-s222 <kouji620222@gmail.com>
2020-08-01 15:56:26 +0900

    incomes移動

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
+  root "incomes#index"
   get 'posts', to: "incomes#new"
 	post "incomes/index"
   resources :users, only: [:edit, :update]
